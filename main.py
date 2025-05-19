8faa5eca23f735ce1681f7c760c77126a49a1e18
MCFeli <mcfeli@gmx.de>
2025-05-19 16:59:59 +0200

    hotfix: make connection not get immediately closed

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def connect():
     """ Connect to the PostgreSQL database server """
     try:
         # connecting to the PostgreSQL server
-        with psycopg.connect(
-                    host="ep-bold-mountain-a2qeaem7-pooler.eu-central-1.aws.neon.tech",
-                    dbname="TINF_ERP",
-                    user="business_logic",
-                    password="npg_K7wOZFcHWo1N"
-            ) as conn:              
-            print('Connected to the PostgreSQL server.')
-            return conn
+        conn = psycopg.connect(
+            host="ep-bold-mountain-a2qeaem7-pooler.eu-central-1.aws.neon.tech",
+            dbname="TINF_ERP",
+            user="business_logic",
+            password="npg_K7wOZFcHWo1N"
+        )
+        print('Connected to the PostgreSQL server.')
+        return conn
     except (psycopg.DatabaseError, Exception) as error:
         print(error)
 
